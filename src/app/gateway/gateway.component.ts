e51be9f8424d984f4e1bbd4773f058985a0944bf
Larson <jianbo1889@gmail.com>
2017-10-31 19:05:32 -0700

    add form

--- src/app/gateway/gateway.component.ts
+++ src/app/gateway/gateway.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { RequestOptions, Request, RequestMethod, Http } from '@angular/http';
+import { MatSnackBar } from '@angular/material';
 
 @Component({
   selector: 'app-gateway',
@@ -9,10 +11,63 @@ import { Component, OnInit } from '@angular/core';
 ]
 })
 export class GatewayComponent implements OnInit {
-  constructor() {
+
+  loggedin60 = false;
+  showunits60form = false;
+  message: any;
+  cookie60: string;
+  allcnntdunitlist60 = [];
+  version60 = false;
+  unitversion60 = [];
+  constructor(
+    private http: Http,
+    public snackBar: MatSnackBar
+  ) {
     
    }
 
   ngOnInit() {
+  }
+  clicklogin60() {
+    this.http.post('api/v5login60', {
+            'username': 'v5root',
+            'password': '!v55Rocks!',
+        }).map(res => {
+            console.log(res.json());
+            console.log('body', res.json().body);
+            const botexdy = res.json().body;
+            const bos = JSON.parse(botexdy);
+
+            console.log('result:' ,res.json().cookie[0]);
+            if (bos.apiStatus.responseStatus == 'status_ok') {
+                this.loggedin60 = true;
+                this.message = 'Success Login!';
+                
+            }else {
+                this.message = bos.apiStatus.responseStatus;
+                this.snackBar.open(this.message,'Got it');
+            }
+            this.cookie60 = res.json().cookie[0];
+    }).subscribe(data => {
+        console.log(data);
+    });
+  }
+  
+  clickallconnectunit60() {
+    this.http.post('api/v5allconnect60', {
+        'cookie': this.cookie60,
+    }).map(res => {
+        const bos = res.json().apiStatus.responseStatus;
+        if (bos == 'status_ok') {
+            this.allcnntdunitlist60 = res.json().apiData;
+            console.log(this.allcnntdunitlist60);
+            this.showunits60form = true;
+            this.version60 = false;
+            this.unitversion60 = [];
+        }else {
+            alert('Need to login first')
+        }
+        console.log(res.json().apiData);
+    }).subscribe();
   }
 }
